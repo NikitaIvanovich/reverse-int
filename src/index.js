6f5551e4db53576d0b908947592d9fe259c59966
Nikita Ivanovich <Nikita__99@mail.ru>
2022-08-01 12:46:22 +0300

    completed reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+     // converting number to string
+  let str = n.toString().split("");
+ 
+  // reversing the string
+  str.reverse();
+   
+ 
+  // converting string to integer
+  n = parseInt(str.join(""))
+ 
+  // returning integer
+  return n;
 }
